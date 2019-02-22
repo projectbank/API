90344456bdfe0bc6469e20a3edb048d445b896ba
Bowero <ik@robinmartijn.nl>
2019-02-22 20:31:19 +0100

    Added example connection

---
+++ conn.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "example";
+$password = "example";
+$database = "example";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+?>
