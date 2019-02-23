1b0e21463930c86e32ef26a86e97037e91ffcb8b
Bowero <ik@robinmartijn.nl>
2019-02-23 11:54:21 +0100

    Add optionality to check credentials. Closes #13.

---
+++ clients/credentials.php
@@ -0,0 +1,24 @@
+<?php
+
+header('Content-Type: application/json');
+
+/*
+To sanitize the input, we:
+
+1. strip the whitespace
+2. replace all html characters
+
+After we have done that, we will check if all of the requirements are met.
+This will be done for each specific case, so we can return specific error codes.
+ */
+
+$nuid = str_replace(' ', '', htmlspecialchars($_GET['nuid']));
+$pin = str_replace(' ', '', htmlspecialchars($_GET['pin']));
+
+$client = new Client($nuid, $pin);
+
+$response = $client->checkLogin();
+
+echo json_encode(array('status' => $response));
+
+?>
