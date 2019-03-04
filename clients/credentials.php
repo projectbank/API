541035019aeba91727a84b0a42eea5e900692c6b
Robin Martijn <ik@robinmartijn.nl>
2019-03-04 14:58:41 +0100

    Update credentials.php

--- clients/credentials.php
+++ clients/credentials.php
@@ -14,8 +14,8 @@ After we have done that, we will check if all of the requirements are met.
 This will be done for each specific case, so we can return specific error codes.
  */
 
-$nuid = str_replace(' ', '', htmlspecialchars($_GET['nuid']));
-$pin = str_replace(' ', '', htmlspecialchars($_GET['pin']));
+$nuid = str_replace(' ', '', htmlspecialchars($_POST['nuid']));
+$pin = str_replace(' ', '', htmlspecialchars($_POST['pin']));
 
 $client = new Client($nuid, $pin);
 
